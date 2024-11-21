0d4e250fca722c217c73c835ae970fea989adb84
Anjali Patidar <anjali.ckpatidar@gmail.com>
2024-11-20 22:55:45 -0500

    changes

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { MatCardModule } from '@angular/material/card';
+import { Project, ProjectService } from '../services/project.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, MatCardModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  projects: Project[] = [];
+  loading: boolean = true; // Add the loading state
 
+  constructor(private projectService: ProjectService) { }
+
+  ngOnInit(): void {
+    this.projectService.getProjects().subscribe((data: Project[]) => {
+      this.projects = data;
+    });
+  }
 }
